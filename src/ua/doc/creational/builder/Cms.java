b7f74eb3553cf80484a7db629a8039c2ce95a902
anDrekon <4594400@gmail.com>
2020-10-19 12:50:37 +0300

    Added creational pattern: builder

---
+++ src/ua/doc/creational/builder/Cms.java
@@ -0,0 +1,5 @@
+package ua.doc.creational.builder;
+
+public enum Cms {
+    WORDPRESS, ALIFRESCO
+}
---
+++ src/ua/doc/creational/builder/WebsiteBuilder.java
@@ -0,0 +1,16 @@
+package ua.doc.creational.builder;
+
+public abstract class WebsiteBuilder {
+    Website website;
+
+    void createWebsite(){
+        website = new Website();
+    }
+    abstract void buildName();
+    abstract void buildCms();
+    abstract void buildPrice();
+
+    public Website getWebsite() {
+        return website;
+    }
+}
